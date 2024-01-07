046815b580460b7958817d18a76f0e1143fff7ec
Ludovic FAVRE ARTIGUES <ludovic.favreartigues@gmail.com>
2024-01-07 14:03:36 +0100

    test

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import { mainModule } from 'process'
+
+export default function Home() {
+  return (
+    <main>
+      
+    </main>
+  )
+}
