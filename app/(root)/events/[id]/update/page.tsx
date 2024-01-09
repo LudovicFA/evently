8a48e1e722d118a14a70567b94330cbb8967ee5d
Ludovic FAVRE ARTIGUES <ludovic.favreartigues@gmail.com>
2024-01-09 15:02:59 +0100

    Feat: Homepage et UpdateEvent

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,11 +1,21 @@
 import EventForm from '@/components/shared/EventForm'
+import { getEventById } from '@/lib/actions/event.actions'
+import { UpdateEventParams } from '@/types'
 import { auth } from '@clerk/nextjs'
 import React from 'react'
 
-const UpdateEvent = () => {
+type UpdateEventProp = {
+  params: {
+    id: string,
+  }
+}
+
+const UpdateEvent = async ({params: {id}}: UpdateEventProp) => {
 
     const {sessionClaims} = auth()
     const userId = sessionClaims?.userId as string
+
+    const event = await getEventById(id)
 
   return (
     <>
@@ -14,7 +24,12 @@ const UpdateEvent = () => {
 
         </section>
         <div className='wrapper my-8'>
-            <EventForm userId={userId} type="Update"/>
+            <EventForm 
+              userId={userId} 
+              type="Update" 
+              event={event} 
+              eventId={event._id}
+            />
         </div>
     </>
   )
